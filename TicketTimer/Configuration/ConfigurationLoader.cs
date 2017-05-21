fc1afa61b97fc1626a40485c7c67c460a778c078
Lars Richter <lars.richter@neveling.net>
2017-05-21 21:40:34 +0200

    [master] Some refactoring for the configuration loading.

---
+++ TicketTimer/Configuration/ConfigurationLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+
+namespace TicketTimer.Configuration
+{
+    public class ConfigurationLoader
+    {
+        public static System.Configuration.Configuration GetConfigurationFromFile(string configFilename)
+        {
+            ExeConfigurationFileMap configFileMap =
+                new ExeConfigurationFileMap { ExeConfigFilename = configFilename };
+
+            System.Configuration.Configuration config =
+                ConfigurationManager.OpenMappedExeConfiguration(
+                    configFileMap, ConfigurationUserLevel.None);
+
+            return config;
+        }
+    }
+}
--- TicketTimer/Program.cs
+++ TicketTimer/Program.cs
@@ -1,9 +1,7 @@
 ﻿using ManyConsole;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
-using System.Reflection;
 using TicketTimer.Configuration;
 
 namespace TicketTimer
@@ -29,42 +27,46 @@ namespace TicketTimer
 
         private static IList<ConsoleCommand> GetCommandsFromTypes(List<Type> commandTypes)
         {
-            List<ConsoleCommand> consoleCommandList = new List<ConsoleCommand>();
-            foreach (Type type in (IEnumerable<Type>)commandTypes)
+            var consoleCommands = new List<ConsoleCommand>();
+            foreach (var type in commandTypes)
             {
-                ConstructorInfo constructor = type.GetConstructor(new Type[0]);
+                var constructor = type.GetConstructor(new Type[0]);
                 if (constructor != null)
-                    consoleCommandList.Add((ConsoleCommand)constructor.Invoke(new object[0]));
+                {
+                    consoleCommands.Add((ConsoleCommand)constructor.Invoke(new object[0]));
+                }
             }
-            return consoleCommandList;
+            return consoleCommands;
         }
 
         private static List<Type> GetTypesFromConfiguration()
         {
-            var types = new List<Type>();
-            ExeConfigurationFileMap configFileMap =
-                new ExeConfigurationFileMap { ExeConfigFilename = "Commands.config" };
-
-            System.Configuration.Configuration config =
-                ConfigurationManager.OpenMappedExeConfiguration(
-                    configFileMap, ConfigurationUserLevel.None);
+            var config = ConfigurationLoader.GetConfigurationFromFile("Commands.config");
 
             TicketTimerSection section = (TicketTimerSection)config.GetSection("ticketTimer");
             if (section == null)
             {
                 return new List<Type>(0);
             }
-            foreach (var element in section.Commands.ToList())
+            var commandElements = section.Commands.ToList();
+            var types = ConvertCommandElementsToTypes(commandElements);
+            return types;
+        }
+
+        private static List<Type> ConvertCommandElementsToTypes(List<CommandElement> commandElements)
+        {
+            var types = new List<Type>();
+            foreach (var commandElement in commandElements)
             {
                 try
                 {
-                    var commandType = Type.GetType(element.Type);
+                    var commandType = Type.GetType(commandElement.Type);
                     types.Add(commandType);
                 }
                 catch (Exception exception)
                 {
                     Console.WriteLine(
-                        $"ERROR: Error while loading '{element}' as a type.\n{exception.Message}\n{exception.StackTrace}");
+                        $"ERROR: Error while loading '{commandElement}' as a type.\n{exception.Message}\n{exception.StackTrace}");
                 }
             }
             return types;
