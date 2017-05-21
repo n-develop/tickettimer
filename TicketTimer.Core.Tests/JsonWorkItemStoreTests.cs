d4cc95c52e53559a2b7602ce4078b6fee1170579
Lars Richter <lars.richter@neveling.net>
2017-05-21 13:20:58 +0200

    [master] Added another test for JsonWorkItemStore.

--- TicketTimer.Core.Tests/JsonWorkItemStoreTests.cs
+++ TicketTimer.Core.Tests/JsonWorkItemStoreTests.cs
@@ -37,5 +37,38 @@ namespace TicketTimer.Core.Tests
             var expected = "{\"WorkItems\":[]}";
             Assert.Equal(expected, content);
         }
+
+        [Fact]
+        public void WorkStoreMultipleItems_CreatesCorrectJson()
+        {
+            var memoryFileStore = new MemoryFileStore();
+            var workItemStore = new JsonWorkItemStore(memoryFileStore);
+            workItemStore.Add(new WorkItem
+            {
+                Comment = "",
+                Started = new DateTime(2017, 5, 1, 11, 0, 0),
+                Stopped = new DateTime(2017, 5, 1, 12, 0, 0),
+                TicketNumber = "BDP-1301"
+            });
+
+            workItemStore.Add(new WorkItem
+            {
+                Comment = "abc",
+                Started = new DateTime(2017, 5, 2, 13, 0, 0),
+                Stopped = new DateTime(2017, 5, 2, 14, 0, 0),
+                TicketNumber = "BDP-1302"
+            });
+
+            workItemStore.Save();
+
+            var content = memoryFileStore.ReadFile("unimportant-file-name.txt");
+            var expected = "{\"WorkItems\":[";
+            expected +=
+                "{\"TicketNumber\":\"BDP-1301\",\"Started\":\"2017-05-01T11:00:00\",\"Stopped\":\"2017-05-01T12:00:00\",\"Comment\":\"\"}";
+            expected +=
+                ",{\"TicketNumber\":\"BDP-1302\",\"Started\":\"2017-05-02T13:00:00\",\"Stopped\":\"2017-05-02T14:00:00\",\"Comment\":\"abc\"}";
+            expected += "]}";
+            Assert.Equal(expected, content);
+        }
     }
 }
