e3108e5f43a55402bf0a7952ae0280b00134a0e1
Lars Richter <lars.richter@neveling.net>
2017-05-21 15:11:30 +0200

    [master] Added a start command without functionality.

---
+++ TicketTimer.Core/Commands/StartCommand.cs
@@ -0,0 +1,18 @@
+﻿using ManyConsole;
+using System;
+
+namespace TicketTimer.Core.Commands
+{
+    public class StartCommand : ConsoleCommand
+    {
+        public StartCommand()
+        {
+            IsCommand("start", "Starts the Timer on a given ticket");
+        }
+
+        public override int Run(string[] remainingArguments)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
