4e54ebb685dec7ad7944efe191fdbe006ebb4e70
Lars Richter <lars.richter@neveling.net>
2017-05-22 13:33:39 +0200

    [master] Added Autofac module in core project.

---
+++ TicketTimer.Core/CoreModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using TicketTimer.Core.Infrastructure;
+using TicketTimer.Core.Services;
+
+namespace TicketTimer.Core
+{
+    public class CoreModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<JsonWorkItemStore>().As<WorkItemStore>();
+            builder.RegisterType<LocalFileStore>().As<FileStore>();
+            builder.RegisterType<LocalDateProvider>().As<DateProvider>();
+            builder.RegisterType<WorkItemServiceImpl>().As<WorkItemService>();
+
+            base.Load(builder);
+        }
+    }
+}
