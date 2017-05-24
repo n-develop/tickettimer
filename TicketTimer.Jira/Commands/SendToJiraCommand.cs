909f20c0b7d16a296a905382b8398e2b09d86629
Lars Richter <lars.richter@neveling.net>
2017-05-24 13:15:36 +0200

    [master] Added SendToJiraCommand and JiraService. Not yet implemented.

---
+++ TicketTimer.Jira/Commands/SendToJiraCommand.cs
@@ -0,0 +1,26 @@
+﻿using ManyConsole;
+using TicketTimer.Jira.Services;
+
+namespace TicketTimer.Jira.Commands
+{
+    public class SendToJiraCommand : ConsoleCommand
+    {
+        private readonly JiraService _jiraService;
+
+        public SendToJiraCommand(JiraService jiraService)
+        {
+            _jiraService = jiraService;
+            ConfigureCommand();
+        }
+
+        private void ConfigureCommand()
+        {
+            IsCommand("jira", "Log time for all saved Jira-tickets");
+        }
+
+        public override int Run(string[] remainingArguments)
+        {
+            return 0;
+        }
+    }
+}
---
+++ TicketTimer.Jira/Services/JiraService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TicketTimer.Core.Infrastructure;
+
+namespace TicketTimer.Jira.Services
+{
+    public class JiraService
+    {
+        public void WriteEntireArchive()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void TrackTime(WorkItem workItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
