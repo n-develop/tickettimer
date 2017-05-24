901a597fffa1a73668ef31d49c1e20d17329fb05
Lars Richter <lars.richter@neveling.net>
2017-05-24 13:17:18 +0200

    [master] Added JiraModule for Autofac.

---
+++ TicketTimer.Jira/JiraModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using TicketTimer.Jira.Commands;
+using TicketTimer.Jira.Services;
+
+namespace TicketTimer.Jira
+{
+    public class JiraModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<JiraService>().AsSelf();
+
+            builder.RegisterType<SendToJiraCommand>().AsSelf();
+
+            base.Load(builder);
+        }
+    }
+}
