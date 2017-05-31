0b95e0fa9b424355c173e470a7df5fd12c87f9a0
Lars Richter <github@n-develop.com>
2017-05-31 10:09:52 +0200

    [master] Added "No items" output for show command.

--- TicketTimer.Core/Services/DefaultWorkItemService.cs
+++ TicketTimer.Core/Services/DefaultWorkItemService.cs
@@ -73,11 +73,18 @@ namespace TicketTimer.Core.Services
         public void ShowArchive()
         {
             var archive = _workItemStore.GetState().WorkItemArchive;
-            Console.WriteLine($"|{"Ticket",10}|{"Comment",40}|{"Duration",10}|");
-            Console.WriteLine(new string('-', 64));
-            foreach (var workItem in archive)
+            if (archive.Any())
             {
-                PrintWorkItem(workItem);
+                Console.WriteLine($"|{"Ticket",10}|{"Comment",40}|{"Duration",10}|");
+                Console.WriteLine(new string('-', 64));
+                foreach (var workItem in archive)
+                {
+                    PrintWorkItem(workItem);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No items in the archive.");
             }
         }
 
