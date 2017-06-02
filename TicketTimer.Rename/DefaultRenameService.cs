f38f5ea2cf06c01e6ce9a316696990f9c058b41e
Lars Richter <github@n-develop.com>
2017-06-02 14:14:19 +0200

    [master] Minor progress for rename action.

--- TicketTimer.Rename/DefaultRenameService.cs
+++ TicketTimer.Rename/DefaultRenameService.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using TicketTimer.Core.Services;
 
 namespace TicketTimer.Rename
 {
     public class DefaultRenameService : RenameService
     {
+        private readonly WorkItemService _workItemService;
+
+        public DefaultRenameService(WorkItemService workItemService)
+        {
+            _workItemService = workItemService;
+        }
         public void RenameWorkItem(string currentTicketNumber, string newTicketNumber)
         {
             throw new NotImplementedException();
