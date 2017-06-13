5f5e632f6c80a15a6552eb3cfd997d121e67c57b
Lars Richter <github@n-develop.com>
2017-06-13 22:39:07 +0200

    [master] Implement YoutrackCommand

--- TicketTimer.Youtrack/Commands/YoutrackCommand.cs
+++ TicketTimer.Youtrack/Commands/YoutrackCommand.cs
@@ -1,18 +1,27 @@
-﻿using System;
-using ManyConsole;
+﻿using ManyConsole;
+using TicketTimer.Youtrack.Services;
 
 namespace TicketTimer.Youtrack.Commands
 {
     public class YoutrackCommand : ConsoleCommand
     {
-        public YoutrackCommand()
+        private readonly YoutrackService _youtrackService;
+
+        public YoutrackCommand(YoutrackService youtrackService)
         {
-            
+            _youtrackService = youtrackService;
+            ConfigureCommand();
+        }
+
+        private void ConfigureCommand()
+        {
+            IsCommand("youtrack", "Log time for all saved YouTrack-tickets");
         }
 
         public override int Run(string[] remainingArguments)
         {
-            throw new NotImplementedException();
+            _youtrackService.WriteEntireArchive();
+            return 0;
         }
     }
 }
