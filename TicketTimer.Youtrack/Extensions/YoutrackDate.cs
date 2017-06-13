54c416c2d85e31785319ac1eca03825a590e77a4
Lars Richter <github@n-develop.com>
2017-06-13 22:40:58 +0200

    [master] Added extension methods for converting from and to youtrack date format.

---
+++ TicketTimer.Youtrack/Extensions/YoutrackDate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TicketTimer.Youtrack.Extensions
+{
+    public static class YoutrackDate
+    {
+        private static readonly DateTime BaseDate = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        public static long ToYoutrackDate(this DateTime date)
+        {
+            var utcDate = date.ToUniversalTime();
+            return (long)utcDate.Subtract(BaseDate).TotalMilliseconds;
+        }
+
+        public static DateTime ToDateTime(long youtrackDate)
+        {
+            var utcDate = BaseDate.AddMilliseconds(youtrackDate);
+            return utcDate;
+        }
+    }
+}
