e40c5872f5d82177fee8a643fdb3f77e23f257a5
Lars Richter <github@n-develop.com>
2017-06-13 22:37:04 +0200

    [master] Added YoutrackSharp package.

--- TicketTimer.Youtrack/Services/DefaultYoutrackService.cs
+++ TicketTimer.Youtrack/Services/DefaultYoutrackService.cs
@@ -2,5 +2,9 @@
 {
     public class DefaultYoutrackService : YoutrackService
     {
+        public void WriteEntireArchive()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- TicketTimer.Youtrack/Services/YoutrackService.cs
+++ TicketTimer.Youtrack/Services/YoutrackService.cs
@@ -2,5 +2,6 @@
 {
     public interface YoutrackService
     {
+        void WriteEntireArchive();
     }
 }
